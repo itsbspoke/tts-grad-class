e080d8182544dfbcf5d856c7169f10b50a9df226
Jim Van Fleet <jim@jimvanfleet.com>
2015-01-20 15:23:40 -0500

    Final fix for rsvp specs

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,8 +3,8 @@
 FactoryGirl.define do
   factory :event do
     user
-    starts_at "2014-12-04 17:30:21"
-    ends_at "2014-12-04 18:30:21"
+    starts_at { 2.weeks.from_now }
+    ends_at { 90.minutes.from_now(2.weeks.from_now) }
     title "SierraMaya360"
     description "Co-working integrate pivot big data hacker pitch deck convergence latte earned media co-working actionable insight engaging. Pivot unicorn quantitative vs. qualitative unicorn user centered design minimum viable product fund disrupt. Pivot physical computing sticky note engaging ship it actionable insight moleskine pair programming. Personas thought leader workflow thinker-maker-doer workflow quantitative vs. qualitative 360 campaign personas iterate hacker experiential moleskine. Iterate intuitive cortado viral minimum viable product intuitive thought leader Steve Jobs unicorn paradigm Steve Jobs."
   end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe Group, :type => :model do
     end
     it "should require an owner" do
       expect( build(:group, owner: nil) ).not_to be_valid
-    end    
+    end
+  end
+  describe "unique slug", focus: true do
+    it "should generate a slug when saved" do
+      group = create(:group)
+      expect( group.slug ).not_to be_nil
+    end
+    it "should prevent slug conflict" do
+      group = create(:group)
+      group_two = create(:group)
+      expect( group_two.slug ).not_to be_nil
+      expect( group_two.slug ).not_to eq(group.slug)
+    end
   end
 end
